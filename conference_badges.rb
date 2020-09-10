b5fc7a3cf3981485272cc7a0dc713ce9c28a1fa9
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-10 19:37:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+# Write your code here
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(array)
+  "Hello, my name is #{name}."
+end
+
+badge_maker("Ada")
+
+def batch_badge_creator(array)
+  newarray = []
+  array.each do |name|
+    puts "Hello, my name is #{name}."
+        newarray << name
+  end
+  newarray
+end
+
+batch_badge_creator(a)
